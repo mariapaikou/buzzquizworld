158507ded1eb742e5080c1111cd11f82b9aa7154
Θεοδώρα <ttsochat>
2020-11-30 23:46:45 +0200

    Έφτιαξα μια αστεία κουεστιονσ για τώρα για να δουμε τι παιζει

--- src/Questions.java
+++ src/Questions.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
+/**
+ * This contains a small sample of questions for testing purposes
+ */
 public class Questions {
+    String[][] a;
+    public Questions() {
+
+        a = new String[1][5];
+        a[0][0] = "How much is 2+2= ?";
+        a[0][1] = "1";
+        a[0][2] = "2";
+        a[0][3] = "3";
+        a[0][4] = "4";
+        a[0][5] = "4";
+        a[1][0] = "What is the color of the sky?";
+        a[1][1] = "Brown";
+        a[1][2] = "Blue";
+        a[1][3] = "Yellow";
+        a[1][4] = "Green";
+        a[1][5] = "Blue";
+    }
 
 
 }
--- src/Round.java
+++ src/Round.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.List;
+
 /**
- * Class Round symbolizes a round of the game, which consists of (...) questions. It selects a type of game
+ * Class Round symbolizes a round of the game, which consists of 3 questions. It selects a type of game
  * for the round and the randomized questions.
  */
 public class Round {
+    private static int numberOfQuestions = 3;
+    HashMap<String, List<String>> questions;
+
+    public Round() {
+        for(int i = 0; i < numberOfQuestions; i++)
+            questions.keySet() RandomQuestions[i]
+    }
+
 }

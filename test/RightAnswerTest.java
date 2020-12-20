412531f81f44102402c322b47d9cf2abca5a7e08
ttsochat <ttsochat@csd.auth.gr>
2020-12-20 13:56:54 +0200

    RightAnswerTest done

--- test/RightAnswerTest.java
+++ test/RightAnswerTest.java
@@ -1,14 +1,43 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class RightAnswerTest {
+    private final RightAnswer rightAnswer;
+    private Player A;
+    private Player B;
+    private ArrayList<Player> players;
+
+    public RightAnswerTest(){
+        players = new ArrayList<>();
+        Player A = new Player();
+        Player B = new Player();
+        Player C = new Player();
+        players.add(A);
+        players.add(B);
+        players.add(C);
+        rightAnswer = new RightAnswer();
+        rightAnswer.setPlayersList(players);
+    }
+
+
 
     @Test
     void changePoints() {
+        players.get(0).setStatus(true);
+        players.get(1).setStatus(false);
+        players.get(2).setStatus(true);
+        rightAnswer.changePoints();
+        assertEquals(1000, players.get(0).getScore());
+        assertEquals(0, players.get(1).getScore());
+        assertEquals(1000, players.get(2).getScore());
+
     }
 
     @Test
     void getName() {
+        String name = rightAnswer.getName();
+        assertEquals("RightAnswer", name);
     }
 }

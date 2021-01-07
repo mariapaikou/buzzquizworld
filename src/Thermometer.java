89d9a04e581f6e3b1bce8f10d37c5b498bb41114
mariapaikou <mariapaikou@users.noreply.github.com>
2021-01-07 17:02:42 +0200

    Thermometer ready ThermometerTest ready!!!

--- src/Thermometer.java
+++ src/Thermometer.java
@@ -7,36 +7,76 @@ public class Thermometer extends Type {
 
     private final int points;
     private final HashMap correctAnswers;
-
+    private ArrayList<Player> nameOfWinner;
 
     public Thermometer(){
 
         super();
         points = 5000;
         correctAnswers = new HashMap();
+        nameOfWinner = new ArrayList<>();
 
     }
 
     @Override
     public boolean setPlayersList(ArrayList<Player> players){
         boolean flag = super.setPlayersList(players);
-        for(Player player : players){
-
-            correctAnswers.put(player.getNickname(),0);
-
+        if(flag && players.size()>1){
+            initializeCorrectAnswers();
+        }else{
+            flag = false;
         }
+
         return flag;
     }
 
 
 
+    public void initializeCorrectAnswers(){
+        correctAnswers.clear();
+        for(Player player : players){
+
+            correctAnswers.put(player.getNickname(),0);
+
+        }
+    }
+
+
     public void changePoints(){
 
-        boolean firstToAnswerCorrectly = true;
+        int sup;
 
+        for(Player player : players){
+            if(player.getStatus()){
 
+                sup= (int) correctAnswers.get(player.getNickname());
+                correctAnswers.replace(player.getNickname(),sup+1);
 
+                if((int) correctAnswers.get(player.getNickname()) == 5){
+                    nameOfWinner.add(player);
+                }
 
+            }
+
+        }
+
+        if(nameOfWinner.size() == 1){
+            nameOfWinner.get(0).increaseScoreBy(points);
+            initializeCorrectAnswers();
+        }else if(nameOfWinner.size() > 1){
+            long max = 999999999;
+            sup = -1;
+            for(int i = 0; i<nameOfWinner.size(); i++){
+                if(nameOfWinner.get(i).getClickTime() < max){
+                    max = nameOfWinner.get(i).getClickTime();
+                    sup = i;
+                }
+            }
+            nameOfWinner.get(sup).increaseScoreBy(points);
+            initializeCorrectAnswers();
+        }
+
+        nameOfWinner.clear();
 
 
 
--- test/ThermometerTest.java
+++ test/ThermometerTest.java
@@ -7,17 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ThermometerTest {
 
-    private Player player1;
-    private Player player2;
-    private ArrayList array;
-    private Thermometer thermometer;
+    private final Player player1;
+    private final Player player2;
+    private final ArrayList<Player> array;
+    private final Thermometer thermometer;
 
 
     public ThermometerTest(){
 
         player1 = new Player();
         player2 = new Player();
-        array = new ArrayList();
+        player1.setNickname("maxa");
+        player2.setNickname("bool");
+        array = new ArrayList<>();
         thermometer = new Thermometer();
 
     }
@@ -25,58 +27,109 @@ class ThermometerTest {
     @Test //ok!
     void changePoints() {
 
-        player1.setStatus(true);
-        player2.setStatus(true);
+        player1.setClickTime(500);
+        player2.setClickTime(10000);
+
         array.add(player1);
         array.add(player2);
         thermometer.setPlayersList(array);
 
         for(int i=0; i<6; i++){
+            player1.setStatus(true);
+            player2.setStatus(true);
             thermometer.changePoints();
+
         }
 
         assertEquals(5000,player1.getScore());
+        assertEquals(0,player2.getScore());
+
+        for(int i=0; i<6; i++){
+            player1.setStatus(true);
+            player2.setStatus(true);
+            thermometer.changePoints();
+
+        }
+
+        assertEquals(10000,player1.getScore());
         assertEquals(0,player2.getScore());
     }
 
 
     //  TODO KAINOYRIA CHANGEPOINTS GIA TIS YPOLOIPES PERIPTWSEIS!!
 
-    @Test
+    @Test //ok!
     void changePoints2(){
 
-        player1.setStatus(true);
-        player2.setStatus(true);
+
         array.add(player1);
         array.add(player2);
         thermometer.setPlayersList(array);
+        player1.setClickTime(500);
+        player2.setClickTime(1000);
 
         for(int i = 0; i<= 3; i++){
+            player1.setStatus(true);
+            player2.setStatus(true);
             thermometer.changePoints();
+
         }
 
-        player1.setStatus(false);
-        array.clear();
+
+            player2.setStatus(true);
+            thermometer.changePoints();
+
+
+
+        assertEquals(0,player1.getScore());
+        assertEquals(5000,player2.getScore());
+
+        for(int i = 0; i<= 3; i++){
+            player1.setStatus(true);
+            player2.setStatus(true);
+            thermometer.changePoints();
+
+        }
+
+            player2.setStatus(true);
+            thermometer.changePoints();
+
+        assertEquals(0,player1.getScore());
+        assertEquals(10000,player2.getScore());
+
+
+    }
+
+
+
+    @Test //ok!
+    void changePoints3(){
         array.add(player1);
         array.add(player2);
+
+        player1.setClickTime(6000);
+        player2.setClickTime(5);
+
         thermometer.setPlayersList(array);
 
-        for (int i = 0; i <= 3; i++){
+
+        for(int i = 0 ; i<10 ;i++){
             thermometer.changePoints();
         }
 
         assertEquals(0,player1.getScore());
-        assertEquals(5000,player2.getScore());
+        assertEquals(0,player2.getScore());
 
     }
 
     @Test //ok!
     void setPlayersList(){
 
-        assertEquals(false,thermometer.setPlayersList(array));
+        assertFalse(thermometer.setPlayersList(array));
         array.add(player2);
+        assertFalse(thermometer.setPlayersList(array));
         array.add(player1);
-        assertEquals(true,thermometer.setPlayersList(array));
+        assertTrue(thermometer.setPlayersList(array));
 
     }
 

18ef02774402479e4cd2cb7688b4c7b8583639d8
mariapaikou <mariapaikou@users.noreply.github.com>
2021-01-15 16:48:44 +0200

    Type and TypeTest are ready!

--- src/Type.java
+++ src/Type.java
@@ -1,30 +1,40 @@
 import java.util.ArrayList;
 
 /**
- * This class is the parent of the game types that are available.
- * It has an ArrayList that stores the players.
+ * Type class is and abstract class that represents the type of games.
+ *
  */
-
 public abstract class Type {
 
     protected ArrayList<Player> players;
 
-
+    /**
+     * Constructor
+     */
     public Type() {
 
         this.players = new ArrayList<>();
 
     }
-/*
-    public Type(ArrayList<Player> players) {
-        this.players = players;
-    }
- */
 
+    /**
+     * Abstract method changePoints changes the points of the players.
+     */
     public abstract void changePoints();
 
+    /**
+     * Abstract method getName returns the name of the type of game
+     * @return String
+     */
     public abstract String getName();
 
+
+    /**
+     * Method setPlayerList accepts an arrayList of players. If the arrayList is saved correctly returns true, else
+     * returns false.
+     * @param players the arrayList of players
+     * @return boolean
+     */
     public boolean setPlayersList(ArrayList<Player> players) {
 
         if(players.size() != 0){
@@ -35,7 +45,10 @@ public abstract class Type {
 
     }
 
-
+    /**
+     * Abstract method getExplanation returns the instructions for the type of game.
+     * @return String
+     */
     public abstract String getExplanation();
 
 }
